50c0a89bf2c7e04f5ec4d0356b57a6f769da936b
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-05-22 11:08:02 +0300

    comments update

--- secure-persist/src/main/java/eu/anifantakis/lib/securepersist/PersistManager.kt
+++ secure-persist/src/main/java/eu/anifantakis/lib/securepersist/PersistManager.kt
@@ -53,13 +53,15 @@ class PersistManager(context: Context, keyAlias: String) {
      * @param key The key to store the value under.
      * @param value The value to store.
      *
-     * Example usage:
+     * ```
      *
+     * // Example usage:
      * val encryptedStore = PersistManager(context)
      *
      * encryptedStore.putDataStorePreference("key1", "exampleString")
      * encryptedStore.putDataStorePreference("key2", 123)
      * encryptedStore.putDataStorePreference("key3", true)
+     * ```
      */
     suspend fun <T> putDataStorePreference(key: String, value: T) {
         dataStoreManager.put(key, value)
@@ -72,13 +74,15 @@ class PersistManager(context: Context, keyAlias: String) {
      * @param defaultValue The default value to return if the key does not exist.
      * @return The retrieved value.
      *
-     * Example usage:
+     * ```
      *
+     * // Example usage:
      * val encryptedStore = PersistManager(context)
      *
      * val myStr: String = encryptedStore.getDataStorePreference("key1", "")
      * val myInt: Int = encryptedStore.getDataStorePreference("key2", 0)
      * val myBool: Boolean = encryptedStore.getDataStorePreference("key3", false)
+     * ```
      */
     suspend fun <T : Any> getDataStorePreference(key: String, defaultValue: T): T {
         return dataStoreManager.get(key, defaultValue)
@@ -90,13 +94,15 @@ class PersistManager(context: Context, keyAlias: String) {
      * @param key The key to store the value under.
      * @param value The value to store.
      *
-     * Example usage:
+     * ```
      *
+     * // Example usage:
      * val encryptedStore = PersistManager(context)
      *
      * encryptedStore.encryptDataStorePreference(eStringKey, "encryptedString")
      * encryptedStore.encryptDataStorePreference(eIntKey, 567)
      * encryptedStore.encryptDataStorePreference(eBooleanKey, true)
+     * ```
      */
     suspend fun <T> encryptDataStorePreference(key: String, value: T) {
         dataStoreManager.putEncrypted(key, value)
@@ -109,13 +115,15 @@ class PersistManager(context: Context, keyAlias: String) {
      * @param defaultValue The default value to return if the key does not exist.
      * @return The decrypted value.
      *
-     * Example usage:
+     * ```
      *
+     * // Example usage:
      * val encryptedStore = PersistManager(context)
      *
      * val myStr: String = encryptedStore.decryptDataStorePreference("key1", "")
      * val myInt: Int = encryptedStore.decryptDataStorePreference("key2", 0)
      * val myBool: Boolean = encryptedStore.decryptDataStorePreference("key3", false)
+     * ```
      */
     suspend fun <T> decryptDataStorePreference(key: String, defaultValue: T): T {
         return dataStoreManager.getEncrypted(key, defaultValue)
@@ -126,10 +134,12 @@ class PersistManager(context: Context, keyAlias: String) {
      *
      * @param key The key to delete the value under.
      *
-     * Example usage:
+     * ```
      *
+     * // Example usage:
      * val encryptedStore = PersistManager(context)
      * encryptedStore.deleteDataStorePreference("key1")
+     * ```
      */
     suspend fun deleteDataStorePreference(key: String) {
         dataStoreManager.delete(key)
@@ -157,39 +167,46 @@ class PersistManager(context: Context, keyAlias: String) {
     }
 
     /**
-     * Uses Delegation to set and get encrypted SharedPreferences. Key to the preference is the property name.
+     * Uses Delegation to set and get encrypted SharedPreferences.
+     * Key to the preference is automatically assigned by the variable name.
      *
      * @param defaultValue The default value to return if the key does not exist.
      * @return An EncryptedPreference instance.
      *
-     * Example usage:
+     * ```
      *
-     * val encryptedStore = PersistManager(context)
+     * // Example usage:
+     * val store = PersistManager(context)
      *
-     * var myStr by encryptedStore.preference("delegationString1")
-     * var myInt by encryptedStore.preference(11)
-     * var myBool by encryptedStore.preference(true)
-     *
-     * above the key will be same as the variable name
+     * var myStr by store.preference("delegationString1")
+     * var myInt by store.preference(11)
+     * var myBool by store.preference(true)
+     * ```
      */
     fun <T> preference(defaultValue: T): EncryptedPreference<T> = EncryptedPreference(this, defaultValue)
 
     /**
-     * Uses Delegation to set and get encrypted SharedPreferences.
+     * Uses Delegation to set and get encrypted SharedPreferences and sets the key to the property name.
      *
      * @param key The key to store the value under.
      * @param defaultValue The default value to return if the key does not exist.
      * @return An EncryptedPreference instance.
      *
-     * Example usage:
+     * ```
      *
-     * val encryptedStore = PersistManager(context)
+     * // Example usage:
+     * val store = PersistManager(context)
      *
-     * var myStr by encryptedStore.preference("myStr", "delegationString1")
-     * var myInt by encryptedStore.preference("myInt"", 11)
-     * var myBool by encryptedStore.preference("myBool"", true)
-     *
-     * above the key is irrelevant from the variable name
+     * var myStr by store.preference("myStr", "delegationString1")
+     * var myInt by store.preference("myInt"", 11)
+     * var myBool by store.preference("myBool"", true)
+     * ```
      */
-    fun <T> preference(key: String, defaultValue: T): EncryptedPreference<T> = EncryptedPreference(this, defaultValue, key)
+    fun <T> preference(key: String = "", defaultValue: T): EncryptedPreference<T> {
+        return if (key.trim().isEmpty()) {
+            EncryptedPreference(this, defaultValue, key)
+        } else {
+            EncryptedPreference(this, defaultValue)
+        }
+    }
 }
